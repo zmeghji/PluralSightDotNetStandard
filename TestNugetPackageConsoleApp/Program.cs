0691166e0025838736af6d916d46d4bd09720f65
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-06-01 22:14:22 -0400

    completed module 8

---
+++ TestNugetPackageConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WiredBrainCoffee.Simulator;
+
+namespace TestNugetPackageConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.MakeCappuccino();
+            coffeeMachine.MakeCappuccino();
+            Console.WriteLine($"Cappuccino counter: {coffeeMachine.CounterCappuccino}");
+            coffeeMachine.ShowStoredState();
+        }
+    }
+}
